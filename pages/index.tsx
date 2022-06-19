d6008609d89beab22becb5c29faa9762bb036016
Oğuzhan Yılmaz <me@oguzhan.dev>
2022-06-19 19:24:03 +0300

    button component added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,59 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="w-full max-w-screen-lg mx-auto py-96">
-      <Button>asd</Button>
+    <div>
+      <div className="flex flex-wrap items-start space-x-4 w-full max-w-screen-lg mx-auto py-20">
+        <Button _hierarchy="primary" _size="sm">
+          Button SM
+        </Button>
+        <Button _hierarchy="primary" _size="md">
+          Button MD
+        </Button>
+        <Button _hierarchy="primary" _size="lg">
+          Button LG
+        </Button>
+        <Button _hierarchy="primary" _size="xl">
+          Button XL
+        </Button>
+        <Button _hierarchy="primary" _size="2xl" disabled>
+          Button 2XL
+        </Button>
+      </div>
+      <div className="flex flex-wrap items-start space-x-4 w-full max-w-screen-lg mx-auto py-20">
+        <Button _hierarchy="secondary" _size="sm">
+          Button SM
+        </Button>
+        <Button _hierarchy="secondary" _size="md">
+          Button MD
+        </Button>
+        <Button _hierarchy="secondary" _size="lg">
+          Button LG
+        </Button>
+        <Button _hierarchy="secondary" _size="xl">
+          Button XL
+        </Button>
+        <Button _hierarchy="secondary" _size="2xl" disabled>
+          Button 2XL
+        </Button>
+      </div>
+
+      <div className="flex flex-wrap items-start space-x-4 w-full max-w-screen-lg mx-auto py-20">
+        <Button _hierarchy="error" _size="sm">
+          Button SM
+        </Button>
+        <Button _hierarchy="error" _size="md">
+          Button MD
+        </Button>
+        <Button _hierarchy="error" _size="lg">
+          Button LG
+        </Button>
+        <Button _hierarchy="error" _size="xl">
+          Button XL
+        </Button>
+        <Button _hierarchy="error" _size="2xl" disabled>
+          Button 2XL
+        </Button>
+      </div>
     </div>
   );
 };
