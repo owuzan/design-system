f8aaa8df76bf93eb5e2afbf2075ab0169223a3e6
Oğuzhan Yılmaz <me@oguzhan.dev>
2022-06-19 17:45:21 +0300

    starter packages installed

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Button from "@/components/Button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="w-full max-w-screen-lg mx-auto py-96">
+      <Button>asd</Button>
+    </div>
+  );
+};
+
+export default Home;
